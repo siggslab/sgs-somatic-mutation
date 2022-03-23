e5e3320b715e6dbedb50a610b3c4c5c101daf847
zqiao <qzzbne@gmail.com>
2022-03-23 05:17:26 +0000

    test dataproc

---
+++ mocha/tests/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""Demonstrates the use of the dataproc module."""
+
+import os
+import hailtop.batch as hb
+from cpg_utils.hail import remote_tmpdir
+from analysis_runner import dataproc
+
+service_backend = hb.ServiceBackend(
+    billing_project=os.getenv('HAIL_BILLING_PROJECT'), remote_tmpdir=remote_tmpdir()
+)
+
+batch = hb.Batch(name='dataproc example', backend=service_backend)
+
+
+cluster = dataproc.setup_dataproc(
+    batch,
+    max_age='1h',
+    packages=['click', 'selenium'],
+    init=['gs://cpg-reference/hail_dataproc/install_common.sh'],
+    cluster_name='My Cluster with max-age=1h',
+)
+cluster.add_job('query.py', job_name='example')
+
+
+# Don't wait, which avoids resubmissions if this job gets preempted.
+batch.run(wait=False)
