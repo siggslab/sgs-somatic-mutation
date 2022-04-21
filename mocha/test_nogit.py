a233ccf553cdc413b94a12c667908ef9855e81f5
zqiao <qzzbne@gmail.com>
2022-04-21 02:04:31 +0000

    test

--- mocha/test_nogit.py
+++ mocha/test_nogit.py
@@ -27,17 +27,17 @@ def makeBatch():
 
 b = makeBatch()
 j = b.new_job('checkout_repo')
-prepare_git_job(
-  job=j,
+#prepare_git_job(
+#  job=j,
   # you could specify a name here, like 'analysis-runner'
-  repo_name=get_repo_name_from_current_directory(),
+#  repo_name=get_repo_name_from_current_directory(),
   # you could specify the specific commit here, eg: '1be7bb44de6182d834d9bbac6036b841f459a11a'
-  commit=get_git_commit_ref_of_current_repository(),
-)
+#  commit=get_git_commit_ref_of_current_repository(),
+#)
 
 # Now, the working directory of j is the checkout out repository
 j.command(f'ls ./ &> {j.output_log}')
-b.write_output(j.output_log, output_path("test_git.log"))
+b.write_output(j.output_log, output_path("test_nogit.log"))
 b.run(wait=False)
 
 
