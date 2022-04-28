0ae23fb402c4c81acdbfdbbe745c220843e36cdc
zqiao <qzzbne@gmail.com>
2022-04-28 07:02:50 +0000

    noOut

--- mocha/scripts/mt_vcf.py
+++ mocha/scripts/mt_vcf.py
@@ -31,11 +31,14 @@ def query(dataset, chrom, output, rerun):
                         (mt.GT.is_hom_var() & (ab >= 0.9))) & (mt.DP >= 10) & (mt.GQ >= 20)
         mt2 = hl.variant_qc(mt.filter_entries(filter_condition))
 
-        mt3 = mt2.filter_rows((mt2.variant_qc.AC[0] != 0) & (mt2.variant_qc.AC[1] != 0) & (mt2.variant_qc.call_rate > 0.95))
+        mt3 = mt2.filter_rows((mt2.variant_qc.AC[0] != 0) & (mt2.variant_qc.AC[1] != 0) & (mt2.variant_qc.call_rate >= 0.97) )
 
         mt3_out = mt3.select_rows(mt3.rsid, mt3.qual, mt3.filters, mt3.info )
         mt3_out = mt3_out.select_entries(mt3_out.GT, mt3_out.DP, mt3_out.AD,  mt3_out.GQ )
         mt3_out = mt3_out.annotate_rows(info = mt3_out.info.select("MQ"))
+
+        mt3_out = hl.sample_qc(mt3_out)
+        mt3_out = mt3_out.filter_cols(mt3_out.sample_qc.call_rate >= 0.97)
 
         hl.context.warning("done filter")
         hl.copy_log(log_out)
