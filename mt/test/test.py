3d94b0d3d4392274e71ac192b8b4423447acabba
uqzqiao <qzzbne@gmail.com>
2022-10-04 11:25:03 +1100

    add anyPath

--- mt/test/test.py
+++ mt/test/test.py
@@ -2,12 +2,13 @@ import hail as hl
 import matplotlib.pyplot as plt
 from bokeh.io.export import get_screenshot_as_png
 from cpg_utils.hail import output_path
+from cloudpathlib import AnyPath
 
 def test():
     out = output_path('test.txt')
 
-    with open(out, 'w') as f:
-        f.write('readme')
+    with AnyPath(output_path('test.txt')).open('w+') as f:
+    f.write('test contents')
 
 if __name__ == "__main__":
     test()
