a87fcc9ef52c4710a759fdadc2449b70dcd29754
uqzqiao <qzzbne@gmail.com>
2022-10-04 15:08:37 +1100

    update dataproc

--- mt/sub.sh
+++ mt/sub.sh
@@ -6,3 +6,12 @@ analysis-runner \
 --output-dir "$(whoami)-test" \
 --description "mt call rate test" \
 main.py
+
+# export pVCF
+input="gs://cpg-tob-wgs-test/mt/v7.mt"
+outputDir="mt-call-rate-test"
+chr=22
+analysis-runner --dataset sgs-somatic-mtn --access-level test  --output-dir "$outputDir" --description "mt call rate test on chr22" sub_dataproc.py \
+    --script "shyam.py --dataset '$input' --chrom chr${chr} --output chr${chr}.vcf.bgz" \
+    --jobname convert$chr
+
