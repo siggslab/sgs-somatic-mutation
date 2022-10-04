955ff6556a95ef121aba961efe1846ad6a6eee1a
uqzqiao <qzzbne@gmail.com>
2022-10-04 11:57:01 +1100

    update test.py

--- mt/test/test.py
+++ mt/test/test.py
@@ -1,13 +1,4 @@
-import hail as hl
-from bokeh.io.export import get_screenshot_as_png
 from cloudpathlib import AnyPath
-from cpg_utils.config import get_config
-from cpg_utils.hail_batch import (
-    authenticate_cloud_credentials_in_job,
-    copy_common_env,
-    remote_tmpdir,
-    output_path
-)
 
 def test():
     with AnyPath(output_path('test.txt')).open('w+') as f:
