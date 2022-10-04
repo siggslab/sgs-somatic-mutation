0b35e54f425e51814b3f2135478cb326f83bbdc3
uqzqiao <qzzbne@gmail.com>
2022-10-04 14:39:56 +1100

    update shyam.py

--- mt/shyam.py
+++ mt/shyam.py
@@ -1,45 +1,81 @@
+#!/usr/bin/env python3
 
+import click
 import hail as hl
 import matplotlib.pyplot as plt
 from bokeh.io.export import get_screenshot_as_png
-from cpg_utils.hail import output_path
+from cpg_utils.hail_batch import output_path
 
-TOB_test_data = "gs://cpg-tob-wgs-test/mt/v7.mt"
 def plot_call_rate(mt):
-    """Test script entry point."""
     
-    figure_path = output_path('mt_to_vcf_chr22_test.png')
-    
-    if rerun or not hl.hadoop_exists(figure_path):  
-       percents = [0.0, 0.5, 0.8, 1]
-       callrates = []
-       for per in percents:
-           callrate_mt = mt.filter_rows(mt.variant_qc.call_rate >= per)
-           callrates.append(callrate_mt.count_rows())
+    percents = [0.0, 0.5, 0.8, 1]
+    callrates = []
+    for per in percents:
+        callrate_mt = mt.filter_rows(mt.variant_qc.call_rate >= per)
+        callrates.append(callrate_mt.count_rows())
+    fig = plt.figure()
+    ax = fig.add_axes([0,0,1,1])
+    ax.bar(percents, callrates)
+    ax.set_ylabel('Variants')
+    ax.set_xlabel('Call Rate')
+    figure = plt.show()
+    figure_filename = output_path('mt_call_rate_test.png', 'web')
+    with hl.hadoop_open(figure_filename, 'wb') as f:
+        get_screenshot_as_png(figure).save(f, format='PNG')
 
-       fig = plt.figure()
-       ax = fig.add_axes([0,0,1,1])
-       ax.bar(percents, callrates)
-       ax.set_ylabel('Variants')
-       ax.set_xlabel('Call Rate')
-       plt.show()
-       plt.savefig(figure_path)
+@click.command()
+@click.option('--dataset', help="data to query")
+@click.option('--chrom', help="chromsome")
+@click.option('--output', help="output name")
+@click.option('--rerun', help='Whether to overwrite cached files', default=False)
+def query(dataset, chrom, output, rerun):    
+    p_out = output_path(output)
+    log_out = ouptut_path(output + ".log")
 
-def process():
-    # Subset mt to chr22
-    mt = hl.read_matrix_table(TOB_test_data)
+    hl.init(default_reference='GRCh38')
+    hl.context.warning("inited")
+    hl.copy_log(log_out)
 
-    intervals = ["chr22"]
-    filtered_mt = hl.filter_intervals(mt, [hl.parse_locus_interval(x, reference_genome='GRCh38') for x in intervals])
+    if rerun or not hl.hadoop_exists(p_out):
+        mt = hl.read_matrix_table(dataset)
+        # filter alleles
+        mt = mt.filter_rows((mt.locus.contig == chrom) & (mt.alleles.length() > 1))
+        hl.context.warning("filter")
+        hl.copy_log(log_out)
+        # filter further
+        ab = mt.AD[1] / hl.sum(mt.AD)
+        filter_condition = ((mt.GT.is_hom_ref() & (ab <= 0.1)) |
+                        (mt.GT.is_het() & (ab >= 0.25) & (ab <= 0.75)) |
+                        (mt.GT.is_hom_var() & (ab >= 0.9))) & (mt.DP >= 10) & (mt.GQ >= 20)
+        mt2 = hl.variant_qc(mt.filter_entries(filter_condition))
+        mt3 = mt2.filter_rows((mt2.variant_qc.AC[0] != 0) & (mt2.variant_qc.AC[1] != 0) & (mt2.variant_qc.call_rate >= 0.97) )
+        plot_call_rate(mt3)
 
-    # Densify chr22 sparse matrix
-    filtered_mt = hl.experimental.densify(filtered_mt)
+        mt3_out = mt3.select_rows(mt3.rsid, mt3.qual, mt3.filters)
+        mt3_out = mt3_out.select_entries(mt3_out.GT, mt3_out.DP, mt3_out.AD,  mt3_out.GQ )
 
-    # Run varient QC
-    filtered_mt = hl.variant_qc(filtered_mt)
+        mt3_out = hl.sample_qc(mt3_out)
+        mt3_out = mt3_out.filter_cols(mt3_out.sample_qc.call_rate >= 0.97)
 
-    # Plot the call rate
-    plot_call_rate(filtered_mt)
+        hl.context.warning("done filter")
+        hl.copy_log(log_out)
 
-if __name__ == '__main__':
-    process()  # pylint: disable=no-value-for-parameter
+        metadata = {'filter': {'InbreedingCoeff':{'Description':''},
+            'VQSR':{'Description':''},
+            'AC0':{'Description':''}, 
+            'PASS':{'Description':''}},
+
+            'format': {'AD': {'Description':'AD',
+                'Number':'R',
+                'Type':'Integer'}}
+            }
+ 
+        hl.export_vcf(mt3_out, p_out, metadata=metadata)
+
+    hl.context.warning("done")
+    hl.copy_log(log_out)
+
+
+if __name__ == "__main__":
+    query()
+
