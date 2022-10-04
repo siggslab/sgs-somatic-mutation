aca0157be7098d80f1bdf26d71b6a07c5ee06a8c
uqzqiao <qzzbne@gmail.com>
2022-10-04 12:07:01 +1100

    update test.py

--- mt/test/test.py
+++ mt/test/test.py
@@ -1,8 +1,15 @@
 from cloudpathlib import AnyPath
 
+
+from cpg_utils.hail_batch import (
+    output_path
+)
+
+
 def test():
     with AnyPath(output_path('test.txt')).open('w+') as f:
-    f.write('test contents')
+        f.write('test contents')
+
 
 if __name__ == "__main__":
     test()
