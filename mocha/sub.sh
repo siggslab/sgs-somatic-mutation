be7011083dae0d657e550243e129cc5862eb110a
uqzqiao <qzzbne@gmail.com>
2022-10-05 21:18:30 +1100

    Update sub.sh

--- mocha/sub.sh
+++ mocha/sub.sh
@@ -1,3 +1,13 @@
+## Successful
+input="gs://cpg-tob-wgs-test/mt/v7.mt"
+outputDir="mt-call-rate-test"
+chr=22
+analysis-runner --dataset sgs-somatic-mtn --access-level test  --output-dir "$outputDir" --description "pVCF" sub_dataproc.py \
+    --script "scripts/mt_vcf.py --dataset '$input' --chrom chr${chr} --output Check_chr${chr}.vcf.bgz" \
+    --jobname convert$chr
+
+
+
 #### run export pVCF
 input="gs://cpg-tob-wgs-test/mt/v7.mt"
 outputDir=""
